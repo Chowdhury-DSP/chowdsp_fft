4154f226fa2b630e2474296eccebbdbac0f51254
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2025-06-14 13:48:16 -0700

    Fixing alignment issue in JUCE wrapper (#7)
    * Fixing alignment issue in JUCE wrapper
    
    * Fix missing free

--- chowdsp_fft_juce/chowdsp_fft_juce.cpp
+++ chowdsp_fft_juce/chowdsp_fft_juce.cpp
@@ -31,22 +31,30 @@ public:
 
     void perform (const Complex<float>* input, Complex<float>* output, bool inverse) const noexcept override
     {
+        memcpy (inout_buffer, input, sizeof (float) << (order + 1));
+
         chowdsp::fft::fft_transform (setup_complex,
-                                     (const float*) input,
-                                     (float*) output,
+                                     inout_buffer,
+                                     inout_buffer,
                                      work_buffer,
                                      inverse ? chowdsp::fft::FFT_BACKWARD : chowdsp::fft::FFT_FORWARD);
+
+        memcpy (output, inout_buffer, sizeof (float) << (order + 1));
+
         if (inverse)
             FloatVectorOperations::multiply ((float*) output, 1.0f / float (1 << order), 1 << (order + 1));
     }
 
     void performRealOnlyForwardTransform (float* inoutData, bool ignoreNegativeFreqs) const noexcept override
     {
+        memcpy (inout_buffer, inoutData, sizeof (float) << (order));
         chowdsp::fft::fft_transform (setup_real,
-                                     inoutData,
-                                     inoutData,
+                                     inout_buffer,
+                                     inout_buffer,
                                      work_buffer,
                                      chowdsp::fft::FFT_FORWARD);
+        memcpy (inoutData, inout_buffer, sizeof (float) << (order));
+
         const int nyquist = 1 << (order - 1);
         inoutData[2 * nyquist] = inoutData[1];
         inoutData[2 * nyquist + 1] = 0.0f;
@@ -65,11 +73,15 @@ public:
         JUCE_BEGIN_IGNORE_WARNINGS_MSVC (4334)
         inoutData[1] = inoutData[1 << order];
         JUCE_END_IGNORE_WARNINGS_MSVC
+
+        memcpy (inout_buffer, inoutData, sizeof (float) << (order));
         chowdsp::fft::fft_transform (setup_real,
-                                     inoutData,
-                                     inoutData,
+                                     inout_buffer,
+                                     inout_buffer,
                                      work_buffer,
                                      chowdsp::fft::FFT_BACKWARD);
+        memcpy (inoutData, inout_buffer, sizeof (float) << (order));
+
         FloatVectorOperations::multiply (inoutData, 1.0f / float (1 << order), 1 << order);
     }
 
@@ -77,6 +89,7 @@ public:
     {
         chowdsp::fft::fft_destroy_setup (setup_real);
         chowdsp::fft::fft_destroy_setup (setup_complex);
+        chowdsp::fft::aligned_free (inout_buffer);
         chowdsp::fft::aligned_free (work_buffer);
     }
 
@@ -89,12 +102,16 @@ private:
         setup_complex = chowdsp::fft::fft_new_setup (1 << order, chowdsp::fft::FFT_COMPLEX);
         jassert (setup_complex != nullptr);
 
+        inout_buffer = (float*) chowdsp::fft::aligned_malloc (sizeof (float) << (order + 1));
+        jassert (inout_buffer != nullptr);
+
         work_buffer = (float*) chowdsp::fft::aligned_malloc (sizeof (float) << (order + 1));
         jassert (work_buffer != nullptr);
     }
 
     void* setup_real = nullptr;
     void* setup_complex = nullptr;
+    float* inout_buffer = nullptr;
     float* work_buffer = nullptr;
     int order {};
 };
